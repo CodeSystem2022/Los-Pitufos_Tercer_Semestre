ed722544f0785ebadf4758ae627e5d030cd91eed
mayra <urielortega300@gmail.com>
2023-06-05 23:58:59 -0300

    Create 05-03-clase.js
    nnn

---
+++ JavaScrip/TSLeccion05/05-03-clase.js
@@ -0,0 +1,13 @@
+class Persona{
+    constructor(nombre , apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+
+    }
+}
+let Pesona1 = Persona('Martin' , 'Perez');
+console.log(persona1); 
+let persona2= new Persona('carlos' , 'lara' );
+console.log(persona2);
+
+
